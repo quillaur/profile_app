890da5e76cc7e08615416422887bb509d67a6eb7
quillaur <aurelienquillet@gmail.com>
2023-03-30 16:20:27 +0200

    Intermediate push on index.html creation

--- myProfile/landing/views.py
+++ myProfile/landing/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the landing index.")
+    return render(request, "index.html")
